6f45938593944f96627b95881459c79a2e50f4b6
Christian <christian.saturnino@pedbot.com.br>
2024-05-17 16:14:35 -0300

    bug fix

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(methodOverride('_method'));
 app.use(express.json());
 app.use(cors(
   {
-    allowedHeaders: ['*'],
+    allowedHeaders: ['*'], 
     credentials: true,
   }
 ));
@@ -42,12 +42,13 @@ app.get('*', (req, res) => {
   res.status(404).json({ error: 'Page not found' });
 });
 
+
 const sequelize = require('./utils/configSequelizer');
 const User = require('./utils/sequalize');
 
 (async () => {
   try {
-    await sequelize.sync();
+    await sequelize.sync(); 
 
     const adminUser = await User.findOne({ where: { isAdmin: true } });
 
@@ -56,23 +57,23 @@ const User = require('./utils/sequalize');
         name: 'Admin',
         email: 'admin@example.com',
         isAdmin: true,
-        password: 'admin',
+        password: 'admin', 
         telephone: '123456789',
         address: 'Admin Address',
-        city: 'Marilia',
+        city: 'Admin City',
         state: 'SP',
         img: 'https://images.unsplash.com/photo-1588534331122-77ac46322dd2?q=80&w=1879&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       });
-      console.log('Administrator user created successfully! Your password is: admin (please change it as soon as possible)');
+      console.log('Administrator user created successfully! Your passoword is: admin (change your password as soon as possible)');
     } else {
-      console.log('An administrator user already exists in the database.');
+      console.log('There is already an administrator user in the database!');
     }
   } catch (error) {
     console.error('Error creating admin user:', error);
   }
 })();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on the port ${PORT}`);
 });
